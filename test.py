55d716b993473c6049286e5db4e3ec24553aed6b
nagadomi <nagadomi@gmail.com>
2024-10-05 17:08:16 +0900

    Fix benchmark; Use torch.cuda.synchronize()

--- test.py
+++ test.py
@@ -10,8 +10,7 @@ def show(depth):
     TF.to_pil_image(depth).show()
 
 
-def test(img_size):
-    N = 10
+def test(img_size, N):
     model, transform = depth_pro.create_model_and_transforms(
         device=torch.device("cuda"),
         precision=torch.float16,
@@ -26,17 +25,20 @@ def test(img_size):
     prediction = model.infer(image)
     show(prediction["depth"])
 
+    torch.cuda.synchronize()
     t = time.time()
     for _ in range(N):
         prediction = model.infer(image)
+    torch.cuda.synchronize()
     print(round(1.0 / ((time.time() - t) / N), 3), "FPS")
 
 
 if __name__ == "__main__":
+    N = 10
     # on RTX3070ti
-    # 1.911 FPS
-    test(img_size=384)
-    # 4.901 FPS
-    test(img_size=256)
-    # 18.01 FPS
-    test(img_size=128)
+    # 1.857 FPS
+    test(img_size=384, N=N)
+    # 4.291 FPS
+    test(img_size=256, N=N)
+    # 15.63 FPS
+    test(img_size=128, N=N)
